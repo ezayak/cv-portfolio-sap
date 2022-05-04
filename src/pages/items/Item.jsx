0bc5bc3a8bc48e88782abf3bcc5163345c977222
Olena Grynevych <ezayak@gmail.com>
2022-05-04 09:27:33 +0200

    styling

--- src/pages/items/Item.jsx
+++ src/pages/items/Item.jsx
@@ -13,7 +13,7 @@ const Item = (props) => {
                     <img className="activator" src={strMealThumb}  alt={strMeal}/>                                    
                 }
             </div>
-            <div className="card-content">
+            <div className="card-content" style={{minHeight:"150px"}}>
                 <span className="card-title activator grey-text text-darken-4">{strMeal}</span>
             </div>
             <div className="card-action">
